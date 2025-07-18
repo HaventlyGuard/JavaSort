90ad73cd1536c0949a967771d6e010358b688c94
Лямин Тимофей Александрович <ip316s15@csc.sibsutis.ru>
2025-07-18 14:30:44 +0700

    Сделал парсер командной строки и текстовые файлы

--- JavaSort/src/filterapp/cli/CommandLineParser.java
+++ JavaSort/src/filterapp/cli/CommandLineParser.java
@@ -1,4 +1,38 @@
 package filterapp.cli;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CommandLineParser {
+    private String outputPath = "./output";
+    private List<String> inputFiles = new ArrayList<>();
+    private String prefix = "";
+    private boolean appendMode =false;
+    private boolean stats =false;
+    private boolean fullStats = false;
+
+    public  CommandLineParser(String[] args){
+        for(int i=0; i< args.length; i++){
+            switch (args[i]){
+                case "-o":
+                    outputPath = args[++i];
+                    break;
+                case "-p":
+                    prefix = args[++i];
+                    break;
+                case "-f":
+                    fullStats = true;
+                    break;
+                case "-s":
+                    stats = true;
+                    break;
+                case "-a":
+                    appendMode=true;
+                    break;
+                default:
+                    inputFiles.add(args[i]);
+
+            }
+        }
+    }
+
 }
